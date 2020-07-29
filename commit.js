193c10d5817851a2055734aa4742d3e914b678d6
tanya044 <61521631+tanya044@users.noreply.github.com>
2020-07-30 00:02:49 +0530

    Create commit.js

---
+++ commit.js
@@ -0,0 +1,49 @@
+-title: MongoDB Developer Hub
+  url: https://developer.mongodb.com
+  main_url: https://developer.mongodb.com
+  description: >
+    The MongoDB Developer Hub serves as the central location for all MongoDB developer content, programs, and resources.
+  categories:
+    - Blog
+    - Data
+    - Education
+    - Learning
+    - Programming
+    - Technology
+    - Web Development
+  built_by: MongoDB
+  featured: false
+- title: Devnet
+  main_url: https://devnet.hr/
+  url: https://devnet.hr/
+  description: >
+    Business website for web development company.
+  categories:
+    - Web Development
+    - Agency
+  built_by: Devnet
+  built_by_url: https://devnet.hr
+- title: Leanier.com
+  url: https://leanier.com
+  main_url: https://leanier.com
+  description: >
+    Enterprise SaaS Management for Modern IT
+  categories:
+    - Data
+    - Technology
+     - Business
+   built_by: leaniercode
+   featured: false
+ - title: FatihaTV
+   url: https://fatihatv.com
+   main_url: https://fatihatv.com
+   description: A distaction-free platform to consume Islamic content from reputable Youtube Channels
+   categories:
+     - Community
+     - Library
+     - Media
+     - Nonprofit
+     - SEO
+   built_by: TengkuHafidz
+   built_by_url: https://twitter.com/sohafidz
+   featured: false
